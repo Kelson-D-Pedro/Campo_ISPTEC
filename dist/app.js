7147125e52722ce02fddfaeba5831951baa627a9
leonel_the13 <vicor32leonel@gmail.com>
2025-07-12 14:35:23 +0100

    bd 50%

--- dist/app.js
+++ dist/app.js
@@ -4,11 +4,24 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const request_controller_1 = require("./requests/request.controller");
+const request_controller_2 = require("./requests/request.controller");
+const request_controller_3 = require("./requests/request.controller");
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
-app.use("/teste", (req, res) => {
-    res.status(200).json({ message: "Servidor já está a vir bem!" });
-});
-//app.use("admin/all", )
-//app.use("/api", apiRoutes); // Uncomment and define apiRoutes if needed
+// ------- ESTUDANTE -------
+app.use("/estudante", request_controller_1.createEstudante);
+app.use("/estudante/:numeroEstudante", request_controller_1.getEstudanteRequests);
+app.put("/estudante/:id", request_controller_1.updateEstudante);
+app.delete("/estudante/:id", request_controller_1.deleteEstudante);
+// ------- EXTERNO -------
+app.use("/externo", request_controller_2.createExterno);
+app.use("/externo/:email", request_controller_2.getExternoRequests);
+app.use("/externo/:id", request_controller_2.updateExterno);
+app.delete("/externo/:id", request_controller_2.deleteExterno);
+// ------- ADMIN -------
+app.use("/admin/all", request_controller_3.getAllRequests);
+app.use("/admin/all", request_controller_3.deleteAllRequests);
+app.use("/admin/estudante/:id/status", request_controller_3.updateStatusEstudante);
+app.use("/admin/externo/:id/status", request_controller_3.updateStatusExterno);
 exports.default = app;
--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,28 @@
 import express from "express";
+import { createEstudante, getEstudanteRequests, updateEstudante, deleteEstudante } from "./requests/request.controller";
+import { createExterno, getExternoRequests, updateExterno, deleteExterno } from "./requests/request.controller";
+import { getAllRequests, deleteAllRequests, updateStatusEstudante, updateStatusExterno } from "./requests/request.controller";
  
 const app = express();
 
 app.use(express.json());
 
-app.use("/teste", (req, res) => {
-    res.status(200).json({ message: "Servidor já está a vir bem!"});
-});
+// ------- ESTUDANTE -------
+app.use("/estudante",createEstudante);
+app.use("/estudante/:numeroEstudante",getEstudanteRequests);
+app.put("/estudante/:id",updateEstudante);
+app.delete("/estudante/:id",deleteEstudante);
 
-//app.use("admin/all", )
-//app.use("/api", apiRoutes); // Uncomment and define apiRoutes if needed
+// ------- EXTERNO -------
+app.use("/externo",createExterno);
+app.use("/externo/:email",getExternoRequests);
+app.use("/externo/:id",updateExterno);
+app.delete("/externo/:id",deleteExterno);
+
+// ------- ADMIN -------
+app.use("/admin/all",getAllRequests);
+app.use("/admin/all",deleteAllRequests);
+app.use("/admin/estudante/:id/status",updateStatusEstudante);
+app.use("/admin/externo/:id/status",updateStatusExterno);
 
 export default app;
