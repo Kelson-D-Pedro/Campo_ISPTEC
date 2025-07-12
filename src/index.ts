018a2e80f357b55e3a7593bcf9398a3b4f6ceb0b
leonel_the13 <vicor32leonel@gmail.com>
2025-07-12 13:36:57 +0100

    masgo voltou

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { create } from "domain";
+import app from "./app";
+import { createServer } from "http";
+import { response } from "express";
+
+const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
+const server = createServer(app);
+
+server.listen(PORT, "0.0.0.0", () => {
+    console.log(`Servidor rodando na porta http://192.168.100.88:${PORT}`);
+})
